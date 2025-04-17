87050cffeef9430b5ed73b5a8e0dae15f58f4770
ze-pena <jose.pena.dev@gmail.com>
2025-04-17 20:44:23 -0300

    feat: wraps the routing component inside the redux store provider

--- src/routing/index.tsx
+++ src/routing/index.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Default from '@layouts/Default';
 import Home from '@pages/Home';
 import Categories from '@pages/Categories';
