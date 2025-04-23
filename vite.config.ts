6bf5296031928139e2240cf0c09981edbedd9ae5
ze-pena <jose.pena.dev@gmail.com>
2025-04-23 20:07:38 -0300

    build: set the base url according to the enviroment mode variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+
+const env = loadEnv('all', './');
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: env.VITE_ENVIRONMENT_MODE === 'dev' ? '/' : '/filmorama/',
   resolve: {
     alias: {
       '@assets': '/src/assets',
