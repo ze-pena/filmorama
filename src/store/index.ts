f0075b100e73f63626fd357dadad52dcd377d301
ze-pena <jose.pena.dev@gmail.com>
2025-04-22 22:40:38 -0300

    feat: create and add "Search" reducer on app store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import favoritesSlice from './reducers/favorites';
+import searchSlice from './reducers/search';
 
 const store = configureStore({
   reducer: {
     favorites: favoritesSlice,
+    search: searchSlice,
   },
 });
 
