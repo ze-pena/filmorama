1f4df62b76fbe96c6b0058f1da32b8e6c563d427
ze-pena <jose.pena.dev@gmail.com>
2025-04-23 12:03:42 -0300

    feat: create new "Movie" reducer and add him to the main store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import favoritesSlice from './reducers/favorites';
 import searchSlice from './reducers/search';
+import moviesSlice from './reducers/movies';
 
 const store = configureStore({
   reducer: {
     favorites: favoritesSlice,
     search: searchSlice,
+    movies: moviesSlice,
   },
 });
 
