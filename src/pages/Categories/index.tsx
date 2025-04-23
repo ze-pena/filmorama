070e798c1764174cb7178631416d8803aaba19cd
ze-pena <jose.pena.dev@gmail.com>
2025-04-23 12:03:55 -0300

    refactor: remove internal api request and use redux to bring data to the component

--- src/pages/Categories/index.tsx
+++ src/pages/Categories/index.tsx
@@ -1,66 +1,55 @@
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-import tmdbServices from '@services/TMDB';
+import { useDispatch, useSelector } from 'react-redux';
+import { StoreDispatch, StoreState } from '@interfaces/Redux';
+import { getMovieGenre, getMovieDiscover } from '@store/reducers/movies';
 
 import GenreCard from '@components/cards/GenreCard';
 import MovieCard from '@components/cards/MovieCard';
 import SpinnerLoading from '@components/loaders/SpinnerLoading';
 
-import { GenreList } from '@interfaces/Genre';
-import { MovieList } from '@interfaces/Movie';
-
 import './styles.scss';
 
 function Categories() {
+  const storeDispatch = useDispatch<StoreDispatch>();
   const params = useParams();
-  const [genreList, setGenreList] = useState<GenreList | null>(null);
-  const [movieList, setMovieList] = useState<MovieList | null>(null);
-
-  const getMoviesGenreList = useCallback(async () => {
-    setMovieList(null);
-    const response: GenreList = await tmdbServices.genre.getMovieGenre();
-    setGenreList(response);
-  }, [setMovieList, setGenreList]);
-
-  const getCategoryMoviesList = useCallback(
-    async (id: string) => {
-      setGenreList(null);
-      const response: MovieList = await tmdbServices.discover.getMovieDiscover(
-        parseInt(id)
-      );
-      setMovieList(response);
-    },
-    [setGenreList, setMovieList]
+  const { genres, results, isLoading } = useSelector(
+    (state: StoreState) => state.movies
   );
 
   useEffect(() => {
     if (params.id) {
-      getCategoryMoviesList(params.id);
+      storeDispatch(
+        getMovieDiscover({
+          genre: parseInt(params.id),
+          sort: ['popularity.desc'],
+        })
+      );
     } else {
-      getMoviesGenreList();
+      storeDispatch(getMovieGenre());
     }
-  }, [getMoviesGenreList, getCategoryMoviesList, params]);
+  }, [storeDispatch, params]);
 
   return (
     <div className="categories">
       <div className="categories__bottom">
-        {genreList || movieList ? (
-          <ul className="categories__bottom__movie-list">
-            {genreList?.genres.map(genre => (
-              <li key={genre.id}>
-                <GenreCard {...genre} />
-              </li>
-            ))}
-
-            {movieList?.results.map(movie => (
-              <li key={movie.id}>
-                <MovieCard {...movie} />
-              </li>
-            ))}
-          </ul>
-        ) : (
+        {isLoading ? (
           <SpinnerLoading />
+        ) : (
+          <ul className="categories__bottom__movie-list">
+            {params.id
+              ? results.map(movie => (
+                  <li key={movie.id}>
+                    <MovieCard {...movie} />
+                  </li>
+                ))
+              : genres.map(genre => (
+                  <li key={genre.id}>
+                    <GenreCard {...genre} />
+                  </li>
+                ))}
+          </ul>
         )}
       </div>
     </div>
